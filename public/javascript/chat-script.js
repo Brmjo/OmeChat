ad88f3ff1d34f30b51a6c27e8413f952674a9b09
John Bermejo <differcode@gmail.com>
2024-09-30 18:54:18 +0800

    Changes alot

--- public/javascript/chat-script.js
+++ public/javascript/chat-script.js
@@ -3,30 +3,59 @@ const socket = io();
 const messageForm = document.getElementById("messageForm");
 const chatBox = document.getElementById("chatbox");
 
+window.onload = () => {
+    displaySystemMessage("You are now chatting with a stranger.");
+}
+
 messageForm.addEventListener('submit', (event) => {
     event.preventDefault();
     
     const message = document.getElementById("messageInput").value;
     if(!message) return alert("Invalid message!");
     socket.emit('sendMessage', {roomId, message});
+    displayMessage(message, false);
+    message.value = "";
 })
 
 socket.emit('joinRoom', ({ roomId }));
 
 socket.on('recieveMessage', (data) => {
-    displayMessage(data);
-})
+    displayMessage(data, true);
+});
 
-function displayMessage(message) {
+function displayMessage(message, isStranger) {
     const messageBox = document.createElement('div');
-    const name = document.createElement('span');
-    const chat = document.createElement('span');
+    const name = document.createElement('div');
+    const chat = document.createElement('div');
     
-    name.textContent = "Stranger: ";
+    messageBox.classList.add("message-box");
+    name.classList.add("message-box-name");
+    chat.classList.add("message-box-chat");
+    
+    if(isStranger) {
+        name.textContent = "Stranger";
+        messageBox.classList.add("stranger-message-box")
+    }else {
+        name.textContent = "You";
+        messageBox.classList.add("your-message-box");
+    }
     chat.textContent = message;
     
-    messageBox.appendChild(name);
-    messageBox.appendChild(chat);
+    messageBox.prepend(chat);
+    messageBox.prepend(name);
     
-    chatBox.appendChild(messageBox);
+    chatBox.prepend(messageBox);
+}
+
+function displaySystemMessage(message) {
+    const messageBox = document.createElement("div");
+    const systemMessage = document.createElement("p");
+    
+    messageBox.classList.add("message-box");
+    systemMessage.classList.add("message-box-system-message");
+    
+    systemMessage.textContent = message;
+    
+    messageBox.prepend(systemMessage);
+    chatBox.prepend(messageBox);
 }
